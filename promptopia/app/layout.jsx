ef68b6ff8a56c6e265efcafe31f1d94603926e2f
scsx <4182349+scsx@users.noreply.github.com>
2023-11-24 15:26:24 +0000

    Set up working

---
+++ promptopia/app/layout.jsx
@@ -0,0 +1,24 @@
+import '@styles/globals.css'
+import { Children } from 'react'
+
+export const metadate = {
+  title: 'Promptopia',
+  description: 'Discover and Share AI prompts',
+  author: 'SCSX'
+}
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang='en'>
+      <body>
+        <div className='main'>
+          <div className='gradient'></div>
+        </div>
+
+        <main className='app'>{children}</main>
+      </body>
+    </html>
+  )
+}
+
+export default RootLayout
