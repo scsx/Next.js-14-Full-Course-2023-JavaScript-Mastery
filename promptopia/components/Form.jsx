a667b99f61713d2b309c2b57a3defe14b08a081a
scsx <4182349+scsx@users.noreply.github.com>
2023-12-02 00:55:00 +0000

    Form

--- promptopia/components/Form.jsx
+++ promptopia/components/Form.jsx
@@ -43,6 +43,18 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
             className='form_input'
           />
         </label>
+
+        <div className='flex-end mx-3 mb-5 gap-4'>
+          <Link href='/' className='text-gray-500 text-sm'>
+            Cancel
+          </Link>
+          <button
+            type='submit'
+            disabled={submitting}
+            className='px-5 py-1.5 text-sm bg-primary-orange rounded-full text-white'>
+            {submitting ? `${type}ing...` : type}
+          </button>
+        </div>
       </form>
     </div>
   )
