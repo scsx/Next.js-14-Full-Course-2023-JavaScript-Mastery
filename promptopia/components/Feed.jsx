1af472f1bbbd4c18c677fcd4fc2e31514b69eff4
scsx <4182349+scsx@users.noreply.github.com>
2023-12-14 04:12:44 +0000

    Search (from Github) and Preview

--- promptopia/components/Feed.jsx
+++ promptopia/components/Feed.jsx
@@ -1,37 +1,71 @@
 'use client'
 
 import { useState, useEffect } from 'react'
+
 import PromptCard from './PromptCard'
 
-// Component, only used on this file so declared here.
 const PromptCardList = ({ data, handleTagClick }) => {
   return (
     <div className='mt-16 prompt_layout'>
       {data.map((post) => (
-        <PromptCard key={post._id} post={post} handleTagClick={handleTagClick} />
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
       ))}
     </div>
   )
 }
 
 const Feed = () => {
-  const [posts, setPosts] = useState([])
+  const [allPosts, setAllPosts] = useState([])
 
   // Search states
   const [searchText, setSearchText] = useState('')
+  const [searchTimeout, setSearchTimeout] = useState(null)
+  const [searchedResults, setSearchedResults] = useState([])
 
-  const handleSearchChange = (e) => {}
+  const fetchPosts = async () => {
+    const response = await fetch('/api/prompt')
+    const data = await response.json()
+
+    setAllPosts(data)
+  }
 
   useEffect(() => {
-    const fetchPosts = async () => {
-      const response = await fetch('/api/prompt')
-      const data = await response.json()
-
-      setPosts(data)
-    }
-
     fetchPosts()
   }, [])
+
+  const filterPrompts = (searchtext) => {
+    const regex = new RegExp(searchtext, 'i') // 'i' flag for case-insensitive search
+    return allPosts.filter(
+      (item) =>
+        regex.test(item.creator.username) ||
+        regex.test(item.tag) ||
+        regex.test(item.prompt)
+    )
+  }
+
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout)
+    setSearchText(e.target.value)
+
+    // debounce method
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPrompts(e.target.value)
+        setSearchedResults(searchResult)
+      }, 500)
+    )
+  }
+
+  const handleTagClick = (tagName) => {
+    setSearchText(tagName)
+
+    const searchResult = filterPrompts(tagName)
+    setSearchedResults(searchResult)
+  }
 
   return (
     <section className='feed'>
@@ -46,9 +80,15 @@ const Feed = () => {
         />
       </form>
 
-      <PromptCardList data={posts} handleTagClick={() => {}} />
-
-      <p className='mb-10'>Add most used tags here, retrieved and processed from DB.</p>
+      {/* All Prompts */}
+      {searchText ? (
+        <PromptCardList
+          data={searchedResults}
+          handleTagClick={handleTagClick}
+        />
+      ) : (
+        <PromptCardList data={allPosts} handleTagClick={handleTagClick} />
+      )}
     </section>
   )
 }
